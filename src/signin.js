4bb5debb3b42730613fd5356974c9b23d1d885b0
BangNguyen1101 <bangprololfaker@gmail.com>
2024-05-15 16:03:31 +0700

    fix button hien thi password

--- src/signin.js
+++ src/signin.js
@@ -1,13 +1,18 @@
-const input = document.querySelector(".input");
+alert("Page da load xong");
+
+const input = document.getElementById("input");
 const eyeOpen = document.querySelector(".eye-open");
 const eyeClose = document.querySelector(".eye-close");
+
 eyeOpen.addEventListener("click",function(){
     eyeOpen.classList.add("hidden");
     eyeClose.classList.remove("hidden");
     input.setAttribute("type","password");
 });
+
 eyeClose.addEventListener("click",function(){
     eyeOpen.classList.remove("hidden");
     eyeClose.classList.add("hidden");
     input.setAttribute("type", "text");
 });
+
