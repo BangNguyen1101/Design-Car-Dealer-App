f62cb4d267f44897f1b10c8abd218882369b927d
BangNguyen1101 <bangprololfaker@gmail.com>
2024-05-31 23:14:07 +0700

    fix lai js hien thi password

--- src/visibility_btn.js
+++ src/visibility_btn.js
@@ -5,12 +5,12 @@ const eyeClose = document.querySelector(".eye-close");
 eyeOpen.addEventListener("click",function(){
     eyeOpen.classList.add("hidden");
     eyeClose.classList.remove("hidden");
-    input.setAttribute("type","password");
+    input.setAttribute("type","text");
 });
 
 eyeClose.addEventListener("click",function(){
     eyeOpen.classList.remove("hidden");
     eyeClose.classList.add("hidden");
-    input.setAttribute("type", "text");
+    input.setAttribute("type", "password");
 });
 
